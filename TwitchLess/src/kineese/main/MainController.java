6ccd692e1abfde81e496ea28aba0bd532cea9569
The_Kineese\Davy <kineese@gmail.com>
2016-12-08 00:36:08 +0100

    Revert "Revert "Initial implementation""
    This reverts commit 4a804b34ec2ab9763ec2f21b5ccf76796d110324.

---
+++ TwitchLess/src/kineese/main/MainController.java
@@ -0,0 +1,13 @@
+package kineese.main;
+
+import kineese.main.controller.ViewController;
+
+public class MainController {
+
+	public static void main(String[] args) {
+		ViewController controller = new ViewController();
+		controller.Start();
+
+	}
+
+}
